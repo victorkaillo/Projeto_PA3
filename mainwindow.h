bd14626c6233f9d7490dee1273f4bce7584436bf
victorkaillo <victorkaillo@hotmail.com>
2017-06-12 08:05:46 -0300

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -2,8 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QTcpSocket>
-#include <QDebug>
 
 namespace Ui {
 class MainWindow;
@@ -12,27 +10,13 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
-private:
-    int id;
+
 public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
 
-    void tcpConnect(QString str);
-public slots:
-  void putData();
-  void xablau(int tempo);
-  void mataTempo();
-  void timerEvent(QTimerEvent *e);
-private slots:
-  void on_buttonConnect_clicked();
-  void on_buttonDisconnect_clicked();
-  void on_buttonStart_clicked();
-  void on_buttonStop_clicked();
-
 private:
   Ui::MainWindow *ui;
-  QTcpSocket *socket;
 };
 
 #endif // MAINWINDOW_H
